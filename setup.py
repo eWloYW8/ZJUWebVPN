53b7c4b3852e7ed6912688248adebf38016edf01
Undefined <3171132517@qq.com>
2025-04-27 12:35:17 +0800

    New version: build and publish test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ZJUWebVPN",
-    version="0.1.1",
+    version="0.1.2",
     author="eWloYW8",
     author_email="3171132517@qq.com",
     description="A Python wrapper for Zhejiang University WebVPN",

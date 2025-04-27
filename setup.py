7f994b8dad8a249a1042cc07fa761017bd6448f5
Undefined <3171132517@qq.com>
2025-04-27 12:31:05 +0800

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ZJUWebVPN",
-    version="0.1.0",
+    version="0.1.1",
     author="eWloYW8",
     author_email="3171132517@qq.com",
     description="A Python wrapper for Zhejiang University WebVPN",
